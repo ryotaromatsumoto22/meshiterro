9bedf7b5d0c97ca1e77f8d573f20d7e13976c05c
vagrant <vagrant@localhost.localdomain>
2020-05-17 02:59:05 +0000

    chapter6-complete

---
+++ app/models/favarite.rb
@@ -0,0 +1,4 @@
+class Favarite < ApplicationRecord
+	belongs_to :user
+    belongs_to :post_image
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_114356) do
+ActiveRecord::Schema.define(version: 2020_05_17_023511) do
+
+  create_table "favarites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
     t.string "image_id"
     t.text "caption"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rost_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
